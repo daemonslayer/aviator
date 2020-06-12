a02dcae9ab3e083477016546e6621484771a9e25
Kapil Gupta <kpgupta98@gmail.com>
2020-06-12 16:42:12 -0700

    Updates

---
+++ precache-manifest.f55d90b3759ae1eee03d3b16fd576186.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f5ede9b2263785e1e8e590f39ff5c45",
+    "url": "/aviator/index.html"
+  },
+  {
+    "revision": "e3f3cda6b32332cbb14e",
+    "url": "/aviator/static/css/main.d36683e5.chunk.css"
+  },
+  {
+    "revision": "2fb0557167b9de6d3593",
+    "url": "/aviator/static/js/2.6c01ada0.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/aviator/static/js/2.6c01ada0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3f3cda6b32332cbb14e",
+    "url": "/aviator/static/js/main.9a00b24a.chunk.js"
+  },
+  {
+    "revision": "e8433fa025273dc3aab6",
+    "url": "/aviator/static/js/runtime-main.450af741.js"
+  }
+]);
